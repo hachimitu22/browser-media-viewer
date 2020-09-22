06f63c51828e7fa783df89a4dfde88c9abe08b68
hachimitu22 <hachimitu22.888@gmail.com>
2020-09-23 03:24:30 +0900

    作者一覧、キャラクター一覧、作品一覧、タグ一覧のrouteを追加

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tagsRouter = require('./routes/tags');
+var charactersRouter = require('./routes/characters');
+var authorsRouter = require('./routes/authors');
+var seriesRouter = require('./routes/series');
 
 var app = express();
 
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tags', tagsRouter);
+app.use('/characters', charactersRouter);
+app.use('/authors', authorsRouter);
+app.use('/series', seriesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
